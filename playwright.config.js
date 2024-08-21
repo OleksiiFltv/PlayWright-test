e5e9617dcdbc358584a16bbdb5a6971e2d32332e
OleksiiFltv <filatov26@ukr.net>
2024-08-21 11:10:55 +0200

    updates

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ const config = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  globalSetup: require.resolve("./loginState.js"),
+  // globalSetup: require.resolve("./loginState.js"),
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
 
